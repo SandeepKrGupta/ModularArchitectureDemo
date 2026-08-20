7e2cc9b719d4076df24d22430cf17b63f958342b
SandeepKrGupta <sg121089@gmail.com>
2026-08-20 16:14:43 +0530

    Initial Commit

---
+++ LoginModularDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginModularDemo
+//
+//  Created by Sandeep on 20/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginModularDemo/LoginModularDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginModularDemoApp.swift
+//  LoginModularDemo
+//
+//  Created by Sandeep on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginModularDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
